36635e0fceaf6d90032a2976d905073508937680
Emilien CHAMPION <5942431-emilienchampion@users.noreply.gitlab.com>
2020-11-22 17:04:26 +0100

    wip

--- app/src/main/java/com/example/sig/MainActivity.kt
+++ app/src/main/java/com/example/sig/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.sig
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sig.views.MapActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, MapActivity::class.java)
+        startActivity(intent)
     }
 }
