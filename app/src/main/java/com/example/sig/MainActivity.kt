0c4ee63a75226d1437647aa3caf26cc4931ce036
Emilien CHAMPION <5942431-emilienchampion@users.noreply.gitlab.com>
2020-12-02 16:16:08 +0100

    wip

--- app/src/main/java/com/example/sig/MainActivity.kt
+++ app/src/main/java/com/example/sig/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.sig
 
+import DemoCollectionAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var demoCollectionAdapter: DemoCollectionAdapter
+    private lateinit var viewPager: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        demoCollectionAdapter = DemoCollectionAdapter()
+        viewPager = findViewById(R.id.activity_main_viewPager)
+        viewPager.adapter = demoCollectionAdapter
+        val tabLayout = findViewById<TabLayout>(R.id.tab_layout)
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = "OBJECT ${(position + 1)}"
+        }.attach()
     }
 }
